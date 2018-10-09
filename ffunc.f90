dbec2acf967f54242cef6bb9a81af39b619fbe85
Danny Hermes <daniel.j.hermes@gmail.com>
2018-10-09 12:19:58 -0700

    Avoid name mangling of Fortran symbol.

--- ffunc.f90
+++ ffunc.f90
@@ -13,7 +13,7 @@
 ! You should have received a copy of the GNU General Public License
 ! along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-     FUNCTION FRTFUNC(STR)
+     FUNCTION FRTFUNC(STR) BIND(C, name='frtfunc')
 
      REAL FRTFUNC
      CHARACTER*(*) STR
